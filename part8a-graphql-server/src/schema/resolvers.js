71d1a284ffd0d33979c4e3b165bb87555ecf9c99
illiaboiko <illiaboiko.095@gmail.com>
2025-10-08 23:53:00 +0300

    task 8.14 done

--- part8a-graphql-server/src/schema/resolvers.js
+++ part8a-graphql-server/src/schema/resolvers.js
@@ -1,31 +1,31 @@
 const Book = require('../models/Book')
 const Author = require('../models/Author')
+const { GraphQLError } = require('graphql')
 
 const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     allBooks: async (root, args) => {
-      //   let filtered = books
+        const filters={}
 
-      //   if (args.author) {
-      //     filtered = filtered.filter((b) => b.author === args.author)
-      //   }
+        if (args.author) {
+            const author = await Author.findOne({ name: args.author})
+            if (!author) return []
+            filters.author = author._id
+        }
 
-      //   if (args.genre) {
-      //     filtered = filtered.filter((b) => b.genres.includes(args.genre))
-      //   }
+        if(args.genre) {
+            filters.genres = {$in: [args.genre]}
+        }
 
-      return Book.find({})
+        return Book.find(filters).populate('author')
+      
     },
     allAuthors: async () => Author.find({}),
   },
   Author: {
-    bookCount: () => {
-      return {
-        bookCount: async () => Book.collection.countDocuments(),
-      }
-    },
+    bookCount: async (root) => Book.countDocuments({ author: root._id }),
   },
   Mutation: {
     addBook: async (root, args) => {
@@ -33,7 +33,7 @@ const resolvers = {
       let author = await Author.findOne({ name: args.author })
       if (!author) {
         console.log('no such author, creating one')
-        author = new Author({name: args.author})
+        author = new Author({ name: args.author })
         try {
           await author.save()
         } catch (error) {
